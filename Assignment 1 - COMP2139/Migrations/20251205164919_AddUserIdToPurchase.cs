6ed99895ef0c064e97d762591fa1bdb6eb4814f6
dev-mcknz <dev.mcknz@gmail.com>
2025-12-05 11:54:46 -0500

    yes

---
+++ Assignment 1 - COMP2139/Migrations/20251205164919_AddUserIdToPurchase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment_1___COMP2139.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToPurchase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Purchases",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_UserId",
+                table: "Purchases",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_AspNetUsers_UserId",
+                table: "Purchases",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_AspNetUsers_UserId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_UserId",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Purchases");
+        }
+    }
+}
