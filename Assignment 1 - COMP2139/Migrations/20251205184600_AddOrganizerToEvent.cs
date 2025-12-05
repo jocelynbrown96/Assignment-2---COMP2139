bd45c25906d61825098960f9ea111568ab96ebd8
dev-mcknz <dev.mcknz@gmail.com>
2025-12-05 13:58:07 -0500

    fixing roles nonsense

---
+++ Assignment 1 - COMP2139/Migrations/20251205184600_AddOrganizerToEvent.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment_1___COMP2139.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrganizerToEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizerId",
+                table: "Events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "OrganizerId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganizerId",
+                table: "Events",
+                column: "OrganizerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId",
+                table: "Events",
+                column: "OrganizerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_OrganizerId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizerId",
+                table: "Events");
+        }
+    }
+}
